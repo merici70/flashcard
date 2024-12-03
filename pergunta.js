bace4856ee8f641d3d7231bbb45444e292c49ff8
merici70 <00001084422530sp@al.educacao.sp.gov.br>
2024-12-03 15:18:19 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital de Machu Picchu?',
+    'Cusco'
+)
+
+criaCartao(
+    'Esporte',
+    'Quem criou o basquete?',
+    'James Naismith'
+)
+
+criaCartao(
+    'Marcas',
+    'Qual foi o primeiro Civic G10?',
+'Civic Touring 2021'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem trouxe o futebol para o Brasil?',
+    'Charles Miller'
+)
